c822488be4be00cd507e875812813d2d7c4bb750
DjDoge11 <farmerpjmc@gmail.com>
2026-03-09 12:56:21 -0700

    initial commit

---
+++ src/card.jsx
@@ -0,0 +1,18 @@
+import "./card.css"
+import { useState } from "react"
+
+function Card({name, description}){
+
+    const [count, setCount] = useState(0);
+
+    return(
+        <div id="Cards">
+            <h2>{name}</h2>
+            <p>Description: {description}</p>
+            <button onClick={() => setCount(count+1)}>Add {name} to cart</button>
+            <button onClick={()=> setCount(0)}>Reset</button>
+            <p>Total {name}: {count}</p>
+        </div>
+    )
+}
+export default Card
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Nav from './nav.jsx'
+import Card from './card.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Nav />
+    <Card name="Food" description="Are you hungry? I know you're hungry" />
+    <Card name="Water" description="Hydrate or Diedrate" />
+    <Card name="Dehydrated Water" description="'Just add water'" />
+    {/* <App /> */}
+  </StrictMode>,
+)
